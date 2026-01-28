2d69833a77c8d5d776027f24deb1bff79757c302
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2026-01-28 23:34:14 +0600

    Some minor changes

--- MERN_Ngnis_Setup/backend/server.js
+++ MERN_Ngnis_Setup/backend/server.js
@@ -18,7 +18,7 @@
 
 // app.use(
 //   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173", // Restrict to frontend origin
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
 //     credentials: true,
 //   })
 // );
@@ -63,9 +63,6 @@
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
 
 
 
